cb5ebf9f4d519146aa76dd8a2436d74cc6751aca
VovaSidorov <VovaCivic@gmail.com>
2020-04-10 00:53:39 +0300

    fetch posts with saga

--- src/index.js
+++ src/index.js
@@ -7,10 +7,16 @@ import {rootReducer} from "./redux/rootReducer";
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk';
 import {spamWordsMiddleware} from "./redux/middleware";
+import createSagaMiddleware from 'redux-saga'
+import {sagaWatcher} from "./redux/sagas";
+
+const saga = createSagaMiddleware()
 
 const store = createStore(rootReducer, compose(
-    applyMiddleware(thunk,spamWordsMiddleware),
+    applyMiddleware(thunk,spamWordsMiddleware,saga),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
+saga.run(sagaWatcher)
 
 ReactDOM.render(
     <Provider store={store}>
